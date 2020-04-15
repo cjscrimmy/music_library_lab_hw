f641e621be568d029f744d6ff9cceb1a70486c02
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-15 16:48:17 +0100

    started artist all method

--- models/artist.rb
+++ models/artist.rb
@@ -19,4 +19,13 @@ class Artist
         values = [@name]
         @id = SqlRunner.run(sql, values)[0]["id"].to_i
     end
+
+    def self.all()
+        sql = "SELECT * FROM artists"
+        artists_array = SqlRunner.run(sql)
+        return artists_array.map 
+    end
+    # order_hashes = SqlRunner.run(sql)
+    # orders = order_hashes.map { |order| PizzaOrder.new( order ) }
+  
 end
