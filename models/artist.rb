608bfa50372ee309caefb9a0d96a6d8eac15c3c8
cjscrimmy <craigscrimgeour@yahoo.com>
2020-04-15 20:20:31 +0100

    add method to delete all artistsand a method to delete all albums

--- models/artist.rb
+++ models/artist.rb
@@ -40,4 +40,9 @@ class Artist
         values =[@name, @id]
         SqlRunner.run(sql, values)
     end
+
+    def self.delete_all()
+        sql = "DELETE FROM artists"
+        SqlRunner.run(sql)
+    end
 end
